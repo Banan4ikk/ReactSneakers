3b37c02bfd6f073958e7913ac6f82db6d6e9f8ef
Konstantin <banan03112000@gmail.com>
2021-08-23 18:46:34 +0600

    [*] End of layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,47 @@
 function App() {
     return (
         <div className="wrapper clear">
+            {/*Корзина*/}
+            <div style={{display: 'none'}} className="overlay">
+                <div className="drawer">
+                    <h2 className={"mb-30 d-flex justify-between"}>Корзина <img className="deleteItem" src="images/deleteCart.svg"/></h2>
+                    <div className={"Items"}>
+                        <div className="cartItem d-flex align-center mt-20 mb-20">
+                            <img className="mr-20" width={133} height={112} src="images/sneakers/1.jpg"/>
+                            <div className="mr-20">
+                                <p className="sneakers-info mb-5">Кроссовки Nike Air Jordan Зеленый</p>
+                                <b>12 999 Руб.</b>
+                            </div>
+                            <img className="deleteItem" src="images/deleteCart.svg"/>
+                        </div>
+                        <div className="cartItem d-flex align-center mt-20 mb-20">
+                            <img className="mr-20" width={133} height={112} src="images/sneakers/1.jpg"/>
+                            <div className="mr-20">
+                                <p className="sneakers-info mb-5">Кроссовки Nike Air Jordan Зеленый</p>
+                                <b>12 999 Руб.</b>
+                            </div>
+                            <img className="deleteItem" src="images/deleteCart.svg"/>
+                        </div>
+                    </div>
+                    <div className={"cardTotalBlock"}>
+                        <ul>
+                            <li>
+                                <span>Итого</span>
+                                <div></div>
+                                <b>21 498 руб.</b>
+                            </li>
+                            <li>
+                                <span>Налог 5%</span>
+                                <div></div>
+                                <b>1074 руб.</b>
+                            </li>
+
+                        </ul>
+                        <button className={"greenButton"}>Оформить заказ <img src={"images/arrow.svg"}/></button>
+                    </div>
+                </div>
+            </div>
+            {/*Корзина конец*/}
             <header className="d-flex justify-between align-center p-40">
                 <div className="headerLeft d-flex align-center">
                     <img width={40} height={40} src="/images/logo.png"/>
@@ -22,10 +63,20 @@ function App() {
                 </div>
             </header>
             <div className="content p-40">
-                <h1 className="mb-40">Все кроссовки</h1>
+                <div className="d-flex align-center justify-between mb-40">
+                    <h1>Все кроссовки</h1>
+                    <div className="search-block d-flex">
+                        <img width={13} height={45} alt="Search" src="images/search.svg"/>
+                        <input placeholder="Поиск"/>
+                    </div>
+                </div>
+                {/*Карточки*/}
                 <div className="d-flex">
 
                     <div className="card">
+                        <div className="favorite">
+                            <img src="images/unliked-heart.svg"/>
+                        </div>
                         <img width={133} height={112} src="images/sneakers/1.jpg"/>
                         <h5>Кроссовки Nike Air Jordan Зеленый</h5>
                         <div className="d-flex justify-between align-center">
